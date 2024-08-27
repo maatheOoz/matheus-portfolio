b49ec9fe06cd75b8ab38f198021d90279328eb57
maatheOoz <matheussantosbmth@gmail.com>
2024-08-27 19:59:15 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/maatheOoz.png" />
-    <Titulo fontSize={20}>Matheus Vieira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={17}>Matheus Vieira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        maatheOoz
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={11}>
+        Desenvolvedor Python
+      </Descricao>
+      <BotaoTema>Troca tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
